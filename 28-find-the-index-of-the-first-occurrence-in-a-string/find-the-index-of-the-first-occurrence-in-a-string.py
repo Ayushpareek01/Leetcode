ea8ce315a0e900a6ea869a01453e313cad634d3c
Ayushpareek01 <148986555+Ayushpareek01@users.noreply.github.com>
2025-01-20 11:09:05 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (47.33%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.py
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,9 +1,23 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        n = len(haystack)
-        m = len(needle)
-        for i in range(n - m + 1):
-            if haystack[i:i+m] == needle:
-                return i
-        return -1
-        
+       # Approach 1
+        #n = len(haystack)
+        #m = len(needle)
+        #for i in range(n - m + 1):
+       #     if haystack[i:i+m] == needle:
+       #         return i
+       # return -1
+    
+        # apporach 2 
+        return haystack.find(needle)
+
+        #apporcah 3
+        # n = len(needle)
+        # m = len(haystack)
+        # for i in range(m):
+            # for j in range(n):
+                # if needle[i] != haystack[i+j]:
+                  # break
+            # if j == n:
+              #return i
+        # return -1
