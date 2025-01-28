1544ca1713ce38fa7f33041333a7562f85df14a2
Ayushpareek01 <148986555+Ayushpareek01@users.noreply.github.com>
2025-01-28 12:03:51 +0530

    Time: 0 ms (100.00%) | Memory: 21.5 MB (23.01%) - LeetSync

---
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.py
@@ -0,0 +1,53 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def get_successor(self,curr):
+        curr = curr.right
+        while curr is not None and curr.left is not None:
+            curr = curr.left
+        return curr
+    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+        if root is None:
+            return root
+        if root.val > key:
+            root.left = self.deleteNode(root.left, key)
+        elif root.val < key:
+            root.right = self.deleteNode(root.right, key)
+        
+        else:
+            if root.left is None:
+                return root.right
+
+            if root.right is None:
+                return root.left
+            succ = self.get_successor(root)
+            root.val = succ.val
+            root.right = self.deleteNode(root.right, succ.val)
+        
+        return root
+    def level(self, root):
+        if not root:
+            return []
+        
+        result = []
+        queue = deque([root])  
+
+        while queue:
+            node = queue.popleft()
+            result.append(node.val)
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+        
+        return result
+
+
+
+
+
+        
